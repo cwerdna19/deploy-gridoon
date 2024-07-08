41a75422a16716d825d0e7684b2d1c2cb1da1f81
cwerdna19 <wcandrew95@gmail.com>
2024-07-07 19:14:28 -0500

    Fixed scripts

--- deploy-gridoon.sh
+++ deploy-gridoon.sh
@@ -4,8 +4,7 @@
 set -e
 
 # Update and upgrade the system
-apt-get update
-apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
 # Install Docker and Git
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -31,13 +30,15 @@ git clone https://github.com/hashtagbowl/Gridoon ~/Gridoon
 
 # Make scripts executable
 chmod +x ~/deploy-gridoon/update-gridoon.sh
-chmod +x ~/Gridoon/create_self_signed_cert.sh
-
 
 # Set alias for update-gridoon so it can be easily ran
 alias update-gridoon='sh ~/deploy-gridoon/update-gridoon.sh'
 
+# Make .env
+cp ~/Gridoon/nginx-certbot.example.env ~/Gridoon/nginx-certbot.env
+
 # Enable UFW and open port 443
+ufw allow 80
 ufw allow 443
 ufw allow 22
 echo "y" | ufw enable
