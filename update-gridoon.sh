6ec34aea409dadb3fb8a8b245a048cfc49b41304
cwerdna19 <wcandrew95@gmail.com>
2024-09-08 16:09:15 -0500

    Fix docker compose yml file name

--- update-gridoon.sh
+++ update-gridoon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git -C ~/Gridoon pull
-docker compose -f ~/Gridoon/docker-compose-init-certs.yml -p gridoon-website up -d
+docker compose -f ~/Gridoon/docker-compose.yml -p gridoon-website up -d
