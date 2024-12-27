e4fef94b16fac50e6ba66dc7e6974137c0bf151b
cwerdna19 <wcandrew95@gmail.com>
2024-12-26 23:30:01 -0600

    Update to actually delete images for rebuild update-gridoon.sh

--- update-gridoon.sh
+++ update-gridoon.sh
@@ -2,4 +2,6 @@
 
 git -C ~/Gridoon pull
 docker compose up -d --no-deps --build nodejs
+docker volume rm gridoon-website_gridoon_data gridoon-website_nginx_secrets
+docker image rm gridoon-website-nodejs jonasal/nginx-certbot
 docker compose -f ~/Gridoon/docker-compose.yml -p gridoon-website up -d
